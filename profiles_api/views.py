95e50cdcd17bf6885b1c9a25af439fbba95a0826
Anupam Chatterjee <tapashri@tapashris-MacBook-Pro.local>
2020-07-23 16:39:51 -0500

    Added post put patch and delete methods to our API view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,16 +1,47 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
-     """Test API View"""
 
-     def get(self, request, format=None):
-         """returns a list of API view features"""
-         an_apiview = [
-            'Uses HTTP methods as function (get, post, patch, put, delete',
-            'Is similar to a traditional django view',
-            'gives you the most control over your application logic',
-            'is mapped manually to our URLs'
-         ]
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
-         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+    def get(self, request, format=None):
+     """returns a list of API view features"""
+     an_apiview = [
+        'Uses HTTP methods as function (get, post, patch, put, delete',
+        'Is similar to a traditional django view',
+        'gives you the most control over your application logic',
+        'is mapped manually to our URLs'
+     ]
+
+     return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+
+        """create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'Method': 'PUT'})
+
+    def patc(self, request, pk=None):
+        """handle a partial update of an object"""
+        return Response({'Method': 'Patch'})
+
+    def delete(self, request, pk=None):
+        """delete an object"""
+        return Response({'Method': 'Delete'})
